da98b4a841ef9c2ef45d7650ae3c29de6bc95e73
agentc0re <agentc0re@gmail.com>
2017-01-05 17:01:14 -0700

    Well I need to hardlink clone them.  I guess thats what works.

--- AssetBundleManager/Utility.cs
+++ AssetBundleManager/Utility.cs
@@ -1,1 +1,83 @@
-../../../Alpenhorn_v4_5.5.0p3_Rebuild/Assets/AssetBundleManager/Utility.cs
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace AssetBundles
+{
+    public class Utility
+    {
+        // Should figure out a way to make this a NON const string.  that way I can impliment the commeneted out line below.
+        //public const string AssetBundlesOutputPath = "Assets/StreamingAssets/";
+        //public const string AssetBundlesOutputPath = "file://" + Application.streamingAssetsPath + "/";
+
+        public static string GetPlatformName()
+        {
+#if UNITY_EDITOR
+            return GetPlatformForAssetBundles(EditorUserBuildSettings.activeBuildTarget);
+#else
+            return GetPlatformForAssetBundles(Application.platform);
+#endif
+        }
+
+#if UNITY_EDITOR
+        private static string GetPlatformForAssetBundles(BuildTarget target)
+        {
+            switch (target)
+            {
+                case BuildTarget.Android:
+                    return "Android";
+#if UNITY_TVOS
+                case BuildTarget.tvOS:
+                    return "tvOS";
+#endif
+                case BuildTarget.iOS:
+                    return "iOS";
+                case BuildTarget.WebGL:
+                    return "WebGL";
+                case BuildTarget.WebPlayer:
+                    return "WebPlayer";
+                case BuildTarget.StandaloneWindows:
+                case BuildTarget.StandaloneWindows64:
+                    return "Windows";
+                case BuildTarget.StandaloneOSXIntel:
+                case BuildTarget.StandaloneOSXIntel64:
+                case BuildTarget.StandaloneOSXUniversal:
+                    return "OSX";
+                // Add more build targets for your own.
+                // If you add more targets, don't forget to add the same platforms to GetPlatformForAssetBundles(RuntimePlatform) function.
+                default:
+                    return null;
+            }
+        }
+#endif
+
+        private static string GetPlatformForAssetBundles(RuntimePlatform platform)
+        {
+            switch (platform)
+            {
+                case RuntimePlatform.Android:
+                    return "Android";
+                case RuntimePlatform.IPhonePlayer:
+                    return "iOS";
+#if UNITY_TVOS
+                case RuntimePlatform.tvOS:
+                    return "tvOS";
+#endif
+                case RuntimePlatform.WebGLPlayer:
+                    return "WebGL";
+                case RuntimePlatform.OSXWebPlayer:
+                case RuntimePlatform.WindowsWebPlayer:
+                    return "WebPlayer";
+                case RuntimePlatform.WindowsPlayer:
+                    return "Windows";
+                case RuntimePlatform.OSXPlayer:
+                    return "OSX";
+                // Add more build targets for your own.
+                // If you add more targets, don't forget to add the same platforms to GetPlatformForAssetBundles(RuntimePlatform) function.
+                default:
+                    return null;
+            }
+        }
+    }
+}
